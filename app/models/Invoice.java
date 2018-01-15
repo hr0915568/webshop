945d5ef52a15bd6c85dd001cf873bf61a5cb9ecd
Jinxi Cheng <0915568@hr.nl>
2018-01-15 14:51:29 +0100

    invoice link added

--- app/models/Invoice.java
+++ app/models/Invoice.java
@@ -50,6 +50,7 @@ public class Invoice extends Model{
     public Float shippingCosts = 0F;
 
     @OneToMany(mappedBy = "invoice")
+    @JsonIgnore
     public List<InvoiceRow> invoiceRows;
 
     public Long getId() {
